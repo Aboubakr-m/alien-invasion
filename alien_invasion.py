7c03c8e155c37aa45bca3002278708a662a6427f
bakr <abobakrmostafa96@gmail.com>
2021-09-30 14:45:22 +0200

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,8 @@ def run_game():
     # Create an instance to store game statistics and create a scoreboard.
     stats = GameStats(ai_settings)
     sb = Scoreboard(ai_settings, screen, stats)
-    # Make a ship, and a group of bullets, , and a group of aliens.
+    
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
